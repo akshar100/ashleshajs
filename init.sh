8b9a68b00f422340a87be033af3f13921dfdfb75
akshar <akshar@akshar.co.in>
2012-12-15 06:02:02 -0800

    Added initialization file

--- init.sh
+++ init.sh
@@ -1,1 +1,18 @@
 #!/bin/sh
+
+PROJECT_HOME=$(pwd);
+npm -v;
+npm install recess connect uglify-js jshint express crypto -g;
+
+cd ashlesha/public/static/bootstrap
+make clean
+make bootstrap
+
+echo "Checking for couchdb"
+
+couchdb
+
+cd $PROJECT_HOME;
+
+echo "Installing node modules "
+npm install
