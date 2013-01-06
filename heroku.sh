6f7353248b4334ecb5296155428511d2fd3b1a11
akshar <akshar@akshar.co.in>
2013-01-05 21:37:04 -0800

    Added Heroku script

--- heroku.sh
+++ heroku.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
 if [ "$1" = "create" ]
-	heroku create "$2"
-	heroku config:add URL="$2"
-elif [ "$2" = "push"]
-	git push heroku master
+then
+    heroku create "$2"
+    heroku config:add URL="$2.herokuapp.com"
+elif [ "$1" = "push" ]
+then
+    git push heroku master
+else
+    echo "Could not understand your command"
 fi
